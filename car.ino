ea0f784d0c5c25885d5fe59fc0f12d3c0ad86286
Tomas Stary <tomichec@gmail.com>
2015-04-10 23:32:29 +0100

    horn implementation

--- car.ino
+++ car.ino
@@ -1,4 +1,34 @@
 #include <SoftwareSerial.h>// import the serial library
+#include "pitches.h"
+
+void
+horn(){
+  // notes in the melody:
+  int melody[] = {
+    NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
+
+  // note durations: 4 = quarter note, 8 = eighth note, etc.:
+  int noteDurations[] = {
+    4, 8, 8, 4,4,4,4,4 };
+
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(8, melody[thisNote],noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
+}
+
+
 
 // seting up the input pins of the motors
 // right motor
@@ -125,6 +155,10 @@ void loop() {
   // go backward right
   if (BluetoothData == 'j'){
     bk_right();
+  }
+  // horn
+  if (BluetoothData == 'V'){
+    horn();
   }
  }  
    //delay(100);
